def7d32d4e135d6768f3ed9045a5cc2c1ed87669
jenchieh <jcs090218@gmail.com>
2018-02-16 00:19:51 +0800

    add ini parser py version core file.

--- src/py/ini_parser.py
+++ src/py/ini_parser.py
@@ -4,7 +4,7 @@
 # $Revision: $
 # $Creator: Jen-Chieh Shen $
 # $Notice: See LICENSE.txt for modification and distribution information
-#                   Copyright © 2018 by Shen, Jen-Chieh $
+#                   Copyright (c) 2018 by Shen, Jen-Chieh $
 # ========================================================================
 
 
@@ -30,14 +30,82 @@ class INI_Parser(object):
     def __init__(self):
         """Constructor."""
 
+        self.in_data = []
+
     #====================
     # Public Methods
+
+    @classmethod
+    def readINIFile(self, filePath):
+        """Read an .ini/.properties file.
+
+        @param { typename } filePath : file path .ini/.properties file.
+        """
+        fd = open(filePath, "r")
+
+        line = fd.readline()
+
+        list = []
+
+        while line:
+            # check comment line.
+            if self.__isCommentLine(line):
+                line = fd.readline()
+                continue
+
+            keyword, value = line.split("=")
+
+            # NOTE(jenchieh): `rstrip' removes trailing newline.
+            list.append(keyword)
+            list.append(value.rstrip())
+
+            line = fd.readline()
+
+        fd.close();
+
+        # assign to current object/memory space.
+        self.in_data = list
+
+        return list
+
+    @classmethod
+    def getProperty(self, keyword):
+        """Get a property from loaded ini file.
+
+        @param { typename } keyword : keyword to search for value.
+        """
+
+        listLen = len(self.in_data)
+        index = 0
+
+        while index < listLen:
+
+            if self.in_data[index] == keyword:
+                return self.in_data[index + 1]
+
+            index += 2
 
     #====================
     # Protected Methods
 
     #====================
     # Private Methods
+
+    @classmethod
+    def __isCommentLine(self, line):
+        """Check if line a comment line.
+
+        @param { typename } line : line to check.
+        """
+
+        for ch in line:
+            if ch != ' ' and ch != '#':
+                return False
+
+            if ch == '#':
+                return True
+
+        return False
 
     #====================
     # setter / getter
