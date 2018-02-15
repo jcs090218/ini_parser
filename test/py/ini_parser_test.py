d1b5bdc41aa270de16db8b7a15cab44a7b82b9b9
jenchieh <jcs090218@gmail.com>
2018-02-15 14:28:53 +0800

    add py ini parser test.

---
+++ test/py/ini_parser_test.py
@@ -0,0 +1,43 @@
+# ========================================================================
+# $File: ini_parser_test.py $
+# $Date: 2018-02-12 18:52:47 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+class INI_Parser_Test(object):
+    """Test if the `INI_Parser' working correctly.
+    """
+
+    #*********************************************#
+    #*             Public Variables              *#
+    #*********************************************#
+
+    #*********************************************#
+    #              Private Variables             *#
+    #*********************************************#
+
+    #*********************************************#
+    #              Protected Variables           *#
+    #*********************************************#
+
+    #*********************************************#
+    #                Constructor                 *#
+    #*********************************************#
+    def __init__(self):
+        """Constructor."""
+
+    #====================
+    # Public Methods
+
+    #====================
+    # Protected Methods
+
+    #====================
+    # Private Methods
+
+    #====================
+    # setter / getter
